6eb5297985c30ec829e5f7a573f307ba8b960510
Ashley Bland <ashleyjbland@gmail.com>
2019-06-03 23:55:27 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,15 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-  get '/' do
+  get '/name' do
     "My name is Ashley"
   end
 
-  get '/' do
+  get '/hometown' do
     "My hometown is Spencer, IN"
   end
 
-  get '/' do
+  get '/favorite-song' do
     "My favorite song is 'YMCA'"
   end
 end
